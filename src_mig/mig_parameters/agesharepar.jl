24fb635089f76b1d0d411ae0f527d7a537a6c9cd
@hbenveniste <helene.benveniste@gmail.com>
2024-12-17 07:17:27 -0800

    redo all for 2024 SSP update versions w/ ad w/o migration

--- src_mig/mig_parameters/agesharepar.jl
+++ src_mig/mig_parameters/agesharepar.jl
@@ -7,95 +7,44 @@ edu = ["e1","e2","e3","e4","e5","e6"]
 
 
 ################## Prepare population data: original SSP ####################
-ssp1 = CSV.File("C:/Users/hmrb/Stanford_Benveniste Dropbox/Hélène Benveniste/YSSP-IIASA/Samir_data/SSP1.csv") |> DataFrame
-ssp2 = CSV.File("C:/Users/hmrb/Stanford_Benveniste Dropbox/Hélène Benveniste/YSSP-IIASA/Samir_data/SSP2.csv") |> DataFrame
-ssp3 = CSV.File("C:/Users/hmrb/Stanford_Benveniste Dropbox/Hélène Benveniste/YSSP-IIASA/Samir_data/SSP3.csv") |> DataFrame
-ssp4 = CSV.File("C:/Users/hmrb/Stanford_Benveniste Dropbox/Hélène Benveniste/YSSP-IIASA/Samir_data/SSP4.csv") |> DataFrame
-ssp5 = CSV.File("C:/Users/hmrb/Stanford_Benveniste Dropbox/Hélène Benveniste/YSSP-IIASA/Samir_data/SSP5.csv") |> DataFrame
-
-select!(ssp1, Not(:Column1))
-select!(ssp2, Not(:Column1))
-select!(ssp3, Not(:Column1))
-select!(ssp4, Not([:Column1, :pattern, :nSx, :pop1, :deaths, :pop2, :pop3, :asfr, Symbol("pop3.shift"), :births, Symbol("deaths.nb"), :pop4, :area]))
-select!(ssp5, Not([:Column1, :pattern, :nSx, :pop1, :deaths, :pop2, :pop3, :asfr, Symbol("pop3.shift"), :births, Symbol("deaths.nb"), :pop4, :area]))
-
-ssp4[!,:scen] = repeat(["SSP4"], size(ssp4,1))
-ssp5[!,:scen] = repeat(["SSP5"], size(ssp5,1))
-
-select!(ssp4, [2,1,3,4,5,6,7,8,9])
-select!(ssp5, [2,1,3,4,5,6,7,8,9])
-
-# Join ssp datasets
-ssp = vcat(ssp1, ssp2, ssp3, ssp4, ssp5)
-
-# Replace missing values by zeros
-for name in [:inmig, :outmig]
-    for i in 1:length(ssp[!,name])
-        if ssp[!,name][i] == "NA" 
-            ssp[!,name][i] = "0"
-        end
-    end
-    ssp[!,name] = map(x -> parse(Float64, x), ssp[!,name])
-end
-
-
-####################### Update SSP population scenarios #################################################
+# Original version:
 # Source:  Wittgenstein Center (WIC) Population and Human Capital Projections, version v.1.3 (February 2024). 
 # https://zenodo.org/records/10618931
-ssp1_update = CSV.read("C:/Users/hmrb/Stanford_Benveniste Dropbox/Hélène Benveniste/YSSP-IIASA/Samir_data/SSP1_V13_2024update.csv", DataFrame)
-ssp2_update = CSV.read("C:/Users/hmrb/Stanford_Benveniste Dropbox/Hélène Benveniste/YSSP-IIASA/Samir_data/SSP2_V13_2024update.csv", DataFrame)
-ssp3_update = CSV.read("C:/Users/hmrb/Stanford_Benveniste Dropbox/Hélène Benveniste/YSSP-IIASA/Samir_data/SSP3_V13_2024update.csv", DataFrame)
-ssp4_update = CSV.read("C:/Users/hmrb/Stanford_Benveniste Dropbox/Hélène Benveniste/YSSP-IIASA/Samir_data/SSP4_V13_2024update.csv", DataFrame)
-ssp5_update = CSV.read("C:/Users/hmrb/Stanford_Benveniste Dropbox/Hélène Benveniste/YSSP-IIASA/Samir_data/SSP5_V13_2024update.csv", DataFrame)
+ssp1 = CSV.read("C:/Users/hmrb/Stanford_Benveniste Dropbox/Hélène Benveniste/YSSP-IIASA/Samir_data/SSP1_V13_2024update.csv", DataFrame)
+ssp2 = CSV.read("C:/Users/hmrb/Stanford_Benveniste Dropbox/Hélène Benveniste/YSSP-IIASA/Samir_data/SSP2_V13_2024update.csv", DataFrame)
+ssp3 = CSV.read("C:/Users/hmrb/Stanford_Benveniste Dropbox/Hélène Benveniste/YSSP-IIASA/Samir_data/SSP3_V13_2024update.csv", DataFrame)
+ssp4 = CSV.read("C:/Users/hmrb/Stanford_Benveniste Dropbox/Hélène Benveniste/YSSP-IIASA/Samir_data/SSP4_V13_2024update.csv", DataFrame)
+ssp5 = CSV.read("C:/Users/hmrb/Stanford_Benveniste Dropbox/Hélène Benveniste/YSSP-IIASA/Samir_data/SSP5_V13_2024update.csv", DataFrame)
 
-ssp1_update.scen = repeat(["SSP1"], size(ssp1_update,1))
-ssp2_update.scen = repeat(["SSP2"], size(ssp2_update,1))
-ssp3_update.scen = repeat(["SSP3"], size(ssp3_update,1))
-ssp4_update.scen = repeat(["SSP4"], size(ssp4_update,1))
-ssp5_update.scen = repeat(["SSP5"], size(ssp5_update,1))
+ssp1.scen = repeat(["SSP1"], size(ssp1,1))
+ssp2.scen = repeat(["SSP2"], size(ssp2,1))
+ssp3.scen = repeat(["SSP3"], size(ssp3,1))
+ssp4.scen = repeat(["SSP4"], size(ssp4,1))
+ssp5.scen = repeat(["SSP5"], size(ssp5,1))
 
-ssp_update = vcat(ssp1_update, ssp2_update, ssp3_update, ssp4_update, ssp5_update)
+ssp = vcat(ssp1, ssp2, ssp3, ssp4, ssp5)
 
-# Full update: use population sizes and demographic distribution of updated scenarios
 # Age: attribute newborns (agest = -5) to the 0-4 year old category
-ssp_update.agest = [ssp_update[i,:agest] == -5 ? 0 : ssp_update[i,:agest] for i in eachindex(ssp_update[:,1])]
-ssp_update = rename(
+ssp.agest = [ssp[i,:agest] == -5 ? 0 : ssp[i,:agest] for i in eachindex(ssp[:,1])]
+ssp = rename(
     combine(
         groupby(
-            ssp_update,
+            ssp,
             [:scen,:Time,:region,:edu,:agest,:sex]
         ),
         :pop => sum, :emi => sum, :imm => sum
     ),
-    :pop_sum => :pop, :emi_sum => :emi, :imm_sum => :imm
+    :Time => :period, :agest => :age, :pop_sum => :pop, :emi_sum => :outmig, :imm_sum => :inmig
 )
 
 # Keep only population numbers for distinct countries (isono < 900)
-ssp_update[!,:region] = map(x -> parse(Int, SubString(x, 4)), ssp_update[!,:region])
+ssp[!,:region] = map(x -> parse(Int, SubString(x, 4)), ssp[!,:region])
 filter!(
     row -> (row.region < 900),
-    ssp_update
+    ssp
 )
 
-ssp_update.sex = [(ssp_update[i,:sex] == "m" ? "male" : "female") for i in eachindex(ssp_update[:,1])]
-
-ssp.region = map(x -> parse(Int, SubString(x, 3)), ssp.region)
-
-ssp = leftjoin(
-    ssp,
-    rename(
-        ssp_update,
-        :Time => :period, :agest => :age, :pop => :pop_update, :emi => :outmig_update, :imm => :inmig_update
-    ),
-    on = [:scen, :period, :region, :edu, :age, :sex]
-)
-
-# Keep only updated versions and rename them 
-select!(ssp, Not([:pop,:outmig,:inmig]))
-rename!(ssp, :pop_update => :pop, :inmig_update => :inmig, :outmig_update => :outmig)
-
-# Remove missing values in the updated version: year 2015, and the Channel Islands
-dropmissing!(ssp)
+ssp.sex = [(ssp[i,:sex] == "m" ? "male" : "female") for i in eachindex(ssp[:,1])]
 
 
 #################################################### Computing age of migrants at time of migration ###################################################
